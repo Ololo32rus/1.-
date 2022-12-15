a3d10bca89ebc3f6662cab3d7c41b66b6922bb7e
Vcyprus32@mail.ru <Vcyprus32@mail.ru>
2022-12-16 01:12:05 +0200

    Добавьте файлы проекта.

---
+++ 1. Белка/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int N, M, K;
+Console.Write("n=");
+N = int.Parse(Console.ReadLine());
+Console.Write("m=");
+M = int.Parse(Console.ReadLine());
+Console.Write("k=");
+K = int.Parse(Console.ReadLine());
+
+if (N * M < K)
+{ Console.WriteLine($"NO"); }
+else
+        { Console.WriteLine($"YES"); }
